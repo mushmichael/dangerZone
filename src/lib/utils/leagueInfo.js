40ca434e29273a622f9ed3d262e1e62ee21a5bbb
Michael Garland <mgarland88@outlook.com>
2022-06-12 01:34:04 -0400

    Adding a username

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -22,7 +22,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
      {
        "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-       "name": "",
+       "name": "mushmichael",
        "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "", // (optional)
        "bio": "stfu",
